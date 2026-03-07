d6b1d77691645c4e5d11c8612f97880ebfcdc049
Sofya Kazykina <kazykinasofia@gmail.com>
2026-03-07 17:23:02 +0300

    Final fixes before merge

--- _deps/googletest-subbuild/CMakeFiles/Makefile.cmake
+++ _deps/googletest-subbuild/CMakeFiles/Makefile.cmake
@@ -7,10 +7,8 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
-  "/Applications/CMake.app/Contents/share/cmake-4.2/Modules/CMakeDetermineSystem.cmake"
   "/Applications/CMake.app/Contents/share/cmake-4.2/Modules/CMakeGenericSystem.cmake"
   "/Applications/CMake.app/Contents/share/cmake-4.2/Modules/CMakeInitializeConfigs.cmake"
-  "/Applications/CMake.app/Contents/share/cmake-4.2/Modules/CMakeSystem.cmake.in"
   "/Applications/CMake.app/Contents/share/cmake-4.2/Modules/CMakeSystemSpecificInformation.cmake"
   "/Applications/CMake.app/Contents/share/cmake-4.2/Modules/CMakeSystemSpecificInitialize.cmake"
   "/Applications/CMake.app/Contents/share/cmake-4.2/Modules/ExternalProject.cmake"
@@ -38,7 +36,6 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
-  "CMakeFiles/4.2.3/CMakeSystem.cmake"
   "googletest-populate-prefix/tmp/googletest-populate-mkdirs.cmake"
   "googletest-populate-prefix/src/googletest-populate-stamp/download-googletest-populate.cmake"
   "googletest-populate-prefix/src/googletest-populate-stamp/verify-googletest-populate.cmake"
