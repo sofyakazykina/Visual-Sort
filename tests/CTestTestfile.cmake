9f1e14fbf75bb094e0ba8ff876719ded7011a767
Sofya <sofya@MacBook-Air-Sofa.local>
2026-03-11 22:35:30 +0300

    Remove CMake

--- tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sofya/CLionProjects/project/tests
-# Build directory: /Users/sofya/CLionProjects/project/Visual-Sort/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[SorterTests]=] "/Users/sofya/CLionProjects/project/Visual-Sort/tests/sorter_test")
-set_tests_properties([=[SorterTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/sofya/CLionProjects/project/tests/CMakeLists.txt;22;add_test;/Users/sofya/CLionProjects/project/tests/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
